0c921f0e5609ad1e598d40c3566cacc74fec5ee8
ijo42 <admin@Ijo42.ru>
2021-11-02 14:27:16 +0500

    initial commit

---
+++ src/main/java/ru/ijo42/autobounds/WeAutoBounds.java
@@ -0,0 +1,18 @@
+package ru.ijo42.autobounds;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WeAutoBounds extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
